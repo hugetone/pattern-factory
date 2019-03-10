d4909c2a713ffcffd7ff85ca515569ae0e7a0e7b
zhaowj <wenji_zhao@126.com>
2019-03-10 22:57:40 +0800

    Initial commit

---
+++ src/main/java/com/gsunis/pattern/factory/MysqlDataBase.java
@@ -0,0 +1,15 @@
+package com.gsunis.pattern.factory;
+
+/**
+ * 描述 ：
+ *
+ * @author : zhaowj
+ * @version : v1.00
+ * @ClassName : MysqlDataBase
+ * @Creation Date : 2019/3/9 10:44 PM
+ * @Description :
+ * @update : 修改人，修改时间，修改内容
+ * @see :[相关类/方法]
+ */
+public class MysqlDataBase {
+}
---
+++ src/main/java/com/gsunis/pattern/factory/simplefactory/DbFactory.java
@@ -0,0 +1,15 @@
+package com.gsunis.pattern.factory.simplefactory;
+
+/**
+ * 描述 ：
+ *
+ * @author : zhaowj
+ * @version : v1.00
+ * @ClassName : DbFactory
+ * @Creation Date : 2019/3/9 10:36 PM
+ * @Description :
+ * @update : 修改人，修改时间，修改内容
+ * @see :[相关类/方法]
+ */
+public class DbFactory {
+}
